cbe059e02424d493b7a6ed28a5efd8b43055ed05
Tran Phan Phuc Nguyen <nguyentppse161945@gmail.com>
2023-05-17 22:45:50 +0700

    Functional Components

--- demoslots247/src/components/Navigation.js
+++ demoslots247/src/components/Navigation.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ 
 import React from 'react'
 export default function Navigation() {
     return (
@@ -11,23 +11,23 @@ export default function Navigation() {
             </ul>
         </nav>
     )
-=======
+ 
 
-import React,{Component} from 'react'
-export default class Navigation extends Component{
-    render(){
-    return(
-        <div>
-            <nav>
-                <ul>
-                    <li><a className='active' href='#home'>Home</a></li>
-                    <li><a href='#news'>News</a></li>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#contact'>Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-    )
-    }
->>>>>>> 6fe581dca7c7b98006552dcfe5db93db74b8d047
-}
+// import React,{Component} from 'react'
+// export default class Navigation extends Component{
+//     render(){
+//     return(
+//         <div>
+//             <nav>
+//                 <ul>
+//                     <li><a className='active' href='#home'>Home</a></li>
+//                     <li><a href='#news'>News</a></li>
+//                     <li><a href='#about'>About</a></li>
+//                     <li><a href='#contact'>Contact</a></li>
+//                 </ul>
+//             </nav>
+//         </div>
+//     )
+//     }
+// >>>>>>> 6fe581dca7c7b98006552dcfe5db93db74b8d047
+ }
