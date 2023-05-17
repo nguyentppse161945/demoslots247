2d1cdc19df8c13e03abf0a2c4b336b5a2b94ba1e
Tran Phan Phuc Nguyen <nguyentppse161945@gmail.com>
2023-05-17 22:04:43 +0700

    hello

---
+++ demoslots247/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a className='active' href='#home'>Home</a></li>
+                <li><a href='#news'>News</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
