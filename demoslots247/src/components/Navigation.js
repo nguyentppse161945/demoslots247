645ef3a13b3ae5c6e15176305449af009a3e2df7
Tran Phan Phuc Nguyen <nguyentppse161945@gmail.com>
2023-06-17 08:10:37 +0700

    ex10

--- demoslots247/src/components/Navigation.js
+++ demoslots247/src/components/Navigation.js
@@ -1,18 +1,13 @@
-
-import React,{Component} from 'react'
-export default class Navigation extends Component{
-    render(){
+import React from 'react'
+export default function Navigation() {
     return(
-        <div>
-            <nav>
-                <ul>
-                    <li><a className='active' href='#home'>Home</a></li>
-                    <li><a href='#news'>News</a></li>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#contact'>Contact</a></li>
-                </ul>
-            </nav>
-        </div>
+        <nav className='navigation'>
+            <ul>
+                <li><a className='active' href='#home'>Home</a></li>
+                <li><a href='#news'>News</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+        </nav>
     )
-    }
 }
