fbd2d1e4748886b57603ac82b161f16d7a33b1a1
Tran Phan Phuc Nguyen <nguyentppse161945@gmail.com>
2023-06-17 10:47:34 +0700

    ex11

--- demoslots247/src/App.js
+++ demoslots247/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
  import Footer from './Footer';
- 
-
+  
 function App() {
+   
   return (
     <div className="App">
     
