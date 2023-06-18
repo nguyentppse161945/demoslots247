abf57304b6163915394e6511459493020b3ac5ab
Tran Phan Phuc Nguyen <nguyentppse161945@gmail.com>
2023-06-18 17:14:02 +0700

    ex13-ex14

---
+++ demoslots247/src/Footer.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import '../src/App.css'
+export default class Navigation extends Component{
+    render(){
+    return(
+        <div className='footer'>
+            <p>copyright @2022</p>
+        </div>
+    )
+    }}
